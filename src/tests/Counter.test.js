b9924585648605a1b02fc329b3a5ccef8530e8c9
Natalie Sesodia <70526708+nsesodia@users.noreply.github.com>
2024-04-08 12:54:37 -0400

    Reset

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -3,20 +3,21 @@
 
 beforeEach(() => {
   // Render the Counter component here
+
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+
 });
